8ccb6e449d1ccf278eac18bcbe1e49b10ffdfa2f
rosalfdz1987 <robertorosalesfdz@gmail.com>
2024-08-19 22:00:16 -0400

    agregue el archivo import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 /* Populate tables */
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (Long rut, String email, String telefono, Date createAt) VALUES('8097788','profesor@bolsadeideas.com','78678' '2017-08-28');
+INSERT INTO clientes (Long rut, String email, String telefono, Date createAt) VALUES('5677848', 'john.doe@gmail.com','45679','2017-08-28');
